64f9faea4e77ae6fa5369f007c63b4c22f0ac819
Anderson Pereira dos Santos <andersonpereiradossantos@outlook.com>
2021-10-07 21:41:22 -0300

    Implementation of sales manipulation methods

---
+++ dotcore-api-minimal/Migrations/20211008003752_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotcore_api_minimal.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Seller = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sales");
+        }
+    }
+}
--- dotcore-api-minimal/Program.cs
+++ dotcore-api-minimal/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<dbContext>();
 
 var app = builder.Build();
 
@@ -10,11 +14,62 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-
 app.UseSwagger();
 
 app.UseSwaggerUI();
     
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/sales", (dbContext db) =>
+{
+    ICollection<Sales> sales = db.Sales.ToList();
+
+    return Results.Ok(sales);
+});
+
+app.MapGet("/sales/{id}", async (long id, dbContext db) =>
+{
+    Sales sales = await db.Sales.FindAsync(id);
+
+    return Results.Ok(sales);
+});
+
+app.MapPost("/sales", async (Sales sales, dbContext db) =>
+{
+    await db.Sales.AddAsync(sales);
+
+    await db.SaveChangesAsync();
+
+    return Results.Created("/sales", sales);
+});
+
+app.MapPut("/sales", async (long id, Sales sale, dbContext db) =>
+{
+    db.Entry(sale).State = EntityState.Modified;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/sales", async (long id, dbContext db) =>
+{
+    Sales sale = await db.Sales.FindAsync(id);
+
+    if(sale != null)
+    {
+        db.Sales.Remove(sale);
+        await db.SaveChangesAsync();
+    }
+
+    return Results.Ok();
+});
 
 app.Run();
+
+public class dbContext : DbContext
+{    public DbSet<Sales> Sales {  get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=YourHost;Database=Company;User Id=YourUser;Password=YourPassword;");
+}
+
+public record Sales(long Id, DateTime DataTime, string Seller, double Price);
